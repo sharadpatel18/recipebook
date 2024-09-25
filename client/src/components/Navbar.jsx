278978ce72f1069fa172d72931db61b8998cd1f7
sharadpatel18 <sharadpatel1807@gmail.com>
2024-09-25 22:42:27 +0530

    other updates

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Outlet, Link } from "react-router-dom";
 
 const Navbar = () => {
+    const getLoginData = () => {
+        const getData = localStorage.getItem('loginData')
+        if (getData) {
+            return JSON.parse(getData)
+        }else{
+            return {}
+        }
+    }
+    const [loginData , setLoginData] = useState(getLoginData())
+ 
+    const handleLogout = () => {
+        localStorage.setItem("loginData" , JSON.stringify({}))
+        window.location.reload()
+    }
+
     return (
         <div>
             <nav className="navbar navbar-expand-lg bg-body-tertiary">
                 <div className="container-fluid">
-                    <Link className="navbar-brand" to="/">username</Link>
+                    {(Object.keys(loginData).length === 0) ? <Link className="navbar-brand" to="/">username</Link> :  <Link className="navbar-brand" to="/">Welcome, {loginData.name}</Link>}
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
@@ -16,7 +31,7 @@ const Navbar = () => {
                                 <Link className="nav-link active" aria-current="page" to="/">Home</Link>
                             </li>
                             <li className="nav-item">
-                                <Link className="nav-link" to="/signup">Signup</Link>
+                               {(Object.keys(loginData).length === 0) ?  <Link className="nav-link" to="/signup">Signup</Link> :  <button onClick={handleLogout}>log out</button>}
                             </li>
                         </ul>
                     </div>
